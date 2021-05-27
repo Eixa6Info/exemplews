fd97711a7b64a630b33f62152515f3c87be951af
Fabien <fabien.brissonneau@gmail.com>
2021-05-27 08:06:42 +0200

    Sources

---
+++ src/test/java/biz/ei6/ws/WsApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.ei6.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
